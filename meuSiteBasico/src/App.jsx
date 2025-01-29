c006051ed027b4ce8286d8fc8dc0f6562fed8cdd
gabims029 <gabimelo541@gmail.com>
2025-01-29 11:26:02 -0300

    Introdução React

---
+++ meuSiteBasico/src/App.jsx
@@ -0,0 +1,9 @@
+import Component1 from "./Component";
+export default function App() {
+  return (
+    <div>
+      <Component1/>
+    </div>
+  );
+}
+
---
+++ meuSiteButton/src/App.jsx
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Bem vindo ao React</h1>
+      <p>Exemplo 2 de componentização</p>
+      <Button/>
+    </div>
+  );
+}
+
+function Button() {
+  function click (){
+    alert("Você clicou no botão");
+  }
+  return (
+    <button
+      style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
+      onClick={()=>click()}
+    >
+      Click aqui
+    </button>
+  );
+}
